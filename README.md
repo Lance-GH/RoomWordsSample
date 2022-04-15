cc4277ae705fd3d605759833fe8db03162e35f5c
Lance <20822340@sun.ac.za>
2022-04-15 21:26:23 +0200

    Added deletion functionality

--- README.md
+++
@@ -1,3 +0,0 @@
-# RoomWordsSample
-An Android app demonstrating the storage of user data by using Room, LiveData and ViewModel. This code was studied from an android fundamentals course
-on the website android.developer.com/courses/ .
--- app/src/main/java/com/example/roomwordssample/MainActivity.java
+++ app/src/main/java/com/example/roomwordssample/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -58,6 +59,30 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setWords(words);
             }
         });
+
+        // Add the functionality to swipe items in the recycler view to delete that item
+        ItemTouchHelper helper = new ItemTouchHelper(
+            new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                @Override
+                public boolean onMove(@NonNull RecyclerView recyclerView,
+                                      @NonNull RecyclerView.ViewHolder viewHolder,
+                                      @NonNull RecyclerView.ViewHolder target) {
+                    return false;
+                }
+
+                @Override
+                public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                    int position = viewHolder.getAdapterPosition();
+                    Word myWord = adapter.getWordAtPosition(position);
+                    Toast.makeText(MainActivity.this, "Deleting " + myWord.getWord(),
+                            Toast.LENGTH_LONG).show();
+
+                    // Delete the word
+                    mWordViewModel.deleteWord(myWord);
+                }
+            });
+
+        helper.attachToRecyclerView(recyclerView);
     }
 
     @Override
@@ -75,7 +100,12 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.clear_data) {
+            // Add a toast just for confirmation
+            Toast.makeText(this, "Clearing the data...", Toast.LENGTH_SHORT).show();
+
+            // Delete the existing data
+            mWordViewModel.deleteAll();
             return true;
         }
 
