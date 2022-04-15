c2229617ef025b99b75ea6805972cd5120fd0b11
Lance <20822340@sun.ac.za>
2022-04-15 19:54:26 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/roomwordssample/WordRepository.java
@@ -0,0 +1,47 @@
+package com.example.roomwordssample;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+// A Repository manages query threads and allows you to use multiple backends. In the most
+// common example, the Repository implements the logic for deciding whether to fetch data from
+// a network or use results cached in the local database.
+
+public class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... words) {
+            mAsyncTaskDao.insert(words[0]);
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/roomwordssample/WordViewModel.java
@@ -0,0 +1,31 @@
+package com.example.roomwordssample;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    // Hides the implementation from the UI
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    // Hides the implementation from the UI
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+
+}
